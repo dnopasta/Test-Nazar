cc05f172fe96d2c32bcff1fdcaf90963574579b0
Maxym Levashov <55034578+dnopasta@users.noreply.github.com>
2024-09-14 21:46:23 +0300

    Add kek script;

---
+++ Test git lol/Assets/kek.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kek : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log("AAAAAAAAA");
+    }
+}
